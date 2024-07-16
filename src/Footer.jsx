47639c07e2d7002519ea8d8e2a6ab52048db18c7
kofta999 <mostafaxxx555@gmail.com>
2024-07-17 00:17:41 +0300

    x

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import './Footer.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer className="footer">
+      <h3>Contact Us</h3>
       <div className="content">
-        <p>Contact us</p>
-    
-        <a href="https://www.instagram.com/padel_station_eg/" target="_blank" rel="noopener noreferrer" className="instagram-link">
-          <FontAwesomeIcon icon={faInstagram} size="2x" color="#ffffff" />
-        </a>
-        <h3>01094469111</h3>
-        <h3>01005754966</h3>
-        <h3>01006039979</h3>
+        <div className="social-links">
+          <a
+            href="https://www.instagram.com/padel_station_eg/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="instagram-link"
+          >
+            <FontAwesomeIcon icon={faInstagram} size="2x" color="#ffffff" />
+          </a>
+        </div>
+
+        <div className="phone">
+          <p>01094469111</p>
+          <p>01005754966</p>
+          <p>01006039979</p>
+        </div>
       </div>
     </footer>
   );
